52aa24122d4ecc9abf07ed23766884c14645f18b
Runbin-tang <54872508+Runbin-tang@users.noreply.github.com>
2020-01-29 10:03:29 +0800

    Add files via upload

---
+++ getting features/start_fre_pos.bat
@@ -0,0 +1,13 @@
+@echo off
+::hiv_dltree_get-feature_position_frequency_new.exe name/refpure.txt refpure 10cg/10cg_5000.txt
+::hiv_dltree_get-feature_position_frequency_new.exe name/refcrf.txt refcrf 10cg/10cg_5000.txt
+::hiv_dltree_get-feature_position_frequency_new.exe name/44reference_cpz_crf.txt 44reference_cpz_crf 10cg/10cg_5000.txt
+::hiv_dltree_get-feature_position_frequency_new.exe name/3D_MoDMaps_name.txt 3D_MoDMaps_name 10cg/10cg_5000.txt
+::hiv_dltree_get-feature_position_frequency_new.exe name/mix_01_A.txt 01 10cg/10cg_5000.txt
+::hiv_dltree_get-feature_position_frequency_new.exe name/mix_02_A_G.txt 02 10cg/10cg_5000.txt
+::hiv_dltree_get-feature_position_frequency_new.exe name/mix_06_A_G_J_K.txt 06 10cg/10cg_5000.txt
+::hiv_dltree_get-feature_position_frequency_new.exe name/mix_35_A_D.txt 35 10cg/10cg_5000.txt
+::hiv_dltree_get-feature_position_frequency_new.exe name/mix_19_A_D_G.txt 19 10cg/10cg_5000.txt
+::hiv_dltree_get-feature_position_frequency_new.exe name/99100newname_cg.txt 99100cg 10cg/10cg_5000.txt
+::hiv_dltree_get-feature_position_frequency_new.exe name/99100newname_pol.txt 99100pol 10cg/10cg_5000.txt
+hiv_dltree_get-feature_position_frequency_new.exe name/5name.txt 5 10cg/10cg_5000.txt
